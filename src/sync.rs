19ee6618e33962e6edad0de7ca923ed203d9d763
Philipp Joram <mail@phijor.me>
2022-02-03 10:55:12 +0200

    docs: License project under `MPL-2.0`

--- src/sync.rs
+++ src/sync.rs
@@ -1,3 +1,7 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 use crate::os::{BorrowHandle, Handle, SystemTick, WeakHandle, CLOSED_HANDLE};
 use crate::result::Result;
 use crate::svc::{self, Timeout};
