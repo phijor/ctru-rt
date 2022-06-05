98eb15960e1a6502177494810ac7d82f09280c17
Philipp Joram <mail@phijor.me>
2022-06-05 23:14:12 +0300

    wip(apt): Remove unnecessary lifetime on `Apt` struct
    `Apt` can be used on its own, but will only exist temporarily. Instead
    `AptLock` is long-lived and protects `AptAccess`.  That in turn allows
    access to instances of `Apt`.

--- src/services/apt.rs
+++ src/services/apt.rs
@@ -2,11 +2,16 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-use core::marker::PhantomData;
+//! Applet Services
+//!
+//! [`Apt`] provides access to the family of [`APT Services`] `APT:{S,A,U}`.
+//!
+//! [`APT Services`]: https://www.3dbrew.org/wiki/NS_and_APT_Services#APT_Services
+
 use core::ops::Deref;
 
 use crate::ipc::{IpcParameter, IpcRequest};
-use crate::os::{AsHandle, OwnedHandle, BorrowedHandle};
+use crate::os::{AsHandle, BorrowedHandle, OwnedHandle};
 use crate::ports::srv::Srv;
 use crate::result::Result;
 use crate::sync::{Event, Mutex, OsMutex};
@@ -42,20 +47,23 @@ impl IpcParameter for AppletAttributes {
     }
 }
 
-pub struct Apt<'access, 'srv> {
-    handle: OwnedHandle,
-    _access: PhantomData<&'access mut AptAccess<'srv>>,
+pub struct AppletManagementInfo {
+    requested_position: AppPosition,
+    requested_id: AppId,
+    menu_id: AppId,
+    active_app_id: AppId,
 }
-impl<'access, 'srv> Apt<'access, 'srv> {
-    fn new(handle: OwnedHandle, access: &'access mut AptAccess<'srv>) -> Self {
-        drop(access);
-        Self {
-            handle,
-            _access: PhantomData,
-        }
+
+pub struct Apt {
+    handle: OwnedHandle,
+}
+
+impl Apt {
+    fn new(handle: OwnedHandle) -> Self {
+        Self { handle }
     }
 
-    fn get_lock(&self, flags: u16) -> Result<OsMutex> {
+    fn get_lock(&mut self, flags: u16) -> Result<OsMutex> {
         let mut reply = IpcRequest::command(0x01)
             .parameter(u32::from(flags))
             .dispatch(&self.handle)?;
@@ -69,83 +77,125 @@ impl<'access, 'srv> Apt<'access, 'srv> {
         Ok(unsafe { OsMutex::from_handle(lock_handle) })
     }
 
-    fn init(&self, app_id: AppId, attributes: AppletAttributes) -> Result<(Event, Event)> {
+    fn init(&mut self, app_id: AppId, attributes: AppletAttributes) -> Result<(Event, Event)> {
         let reply = IpcRequest::command(0x02)
             .parameter(app_id)
             .parameter(attributes)
             .dispatch(&self.handle)?;
 
         let mut reply = reply.finish_results();
-        let [signal_handle, resume_handle]: [OwnedHandle; 2] = unsafe {
-            reply.read_translate_result()
+
+        let events = unsafe {
+            let [signal_handle, resume_handle] = reply.read_handles();
+
+            (
+                Event::from_handle(signal_handle),
+                Event::from_handle(resume_handle),
+            )
         };
 
-        let signal_event = unsafe { Event::from_handle(signal_handle) };
-        let resume_event = unsafe { Event::from_handle(resume_handle) };
-
-        Ok((signal_event, resume_event))
+        Ok(events)
     }
 
-    fn enable(&self, attributes: AppletAttributes) -> Result<()> {
+    fn enable(&mut self, attributes: AppletAttributes) -> Result<()> {
         let _ = IpcRequest::command(0x03)
             .parameter(attributes)
             .dispatch(&self.handle)?;
         Ok(())
     }
+
+    // TODO: Should this consume `self`?
+    fn finalize(&mut self, app_id: AppId) -> Result<()> {
+        let _ = IpcRequest::command(0x04)
+            .parameter(app_id)
+            .dispatch(&self.handle)?;
+
+        Ok(())
+    }
+
+    fn applet_management_info(&mut self, app_id: AppId) -> Result<AppletManagementInfo> {
+        let mut reply = IpcRequest::command(0x05)
+            .parameter(app_id)
+            .dispatch(&self.handle)?;
+
+        let requested_position = AppPosition::from_value(reply.read_word() as u8)
+            .expect("APT returned an invalid App position");
+        let requested_id =
+            AppId::from_value(reply.read_word() as u16).expect("APT returned an invalid App ID");
+        let menu_id = AppId::from_value(reply.read_word() as u16)
+            .expect("APT returned an invalid Menu App ID");
+        let active_app_id = AppId::from_value(reply.read_word() as u16)
+            .expect("APT returned an invalid active App ID");
+
+        Ok(AppletManagementInfo {
+            requested_position,
+            requested_id,
+            menu_id,
+            active_app_id,
+        })
+    }
 }
 
-impl AsHandle for Apt<'_, '_> {
-    fn as_handle(&self) -> BorrowedHandle {
+impl AsHandle for Apt {
+    fn as_handle(&self) -> BorrowedHandle<'_> {
         self.handle.as_handle()
     }
 }
 
-pub struct AptAccess<'srv> {
-    srv: &'srv Srv,
+#[derive(Debug)]
+pub struct AptAccess {
     service_name_index: usize,
 }
 
-impl<'srv> AptAccess<'srv> {
-    fn aquire<'access>(&'access mut self) -> Result<Apt<'access, 'srv>> {
-        let (handle, matched_offset) = self.srv.get_service_handle_alternatives(&APT_SERVICE_NAMES[self.service_name_index..])?;
+impl AptAccess {
+    fn new() -> Self {
+        Self {
+            service_name_index: 0,
+        }
+    }
+
+    fn aquire(&mut self, srv: &Srv) -> Result<Apt> {
+        let (handle, matched_offset) =
+            srv.get_service_handle_alternatives(&APT_SERVICE_NAMES[self.service_name_index..])?;
         self.service_name_index += matched_offset;
 
-        Ok(Apt::new(handle, self))
+        Ok(Apt::new(handle))
     }
 }
 
-pub struct AptLock<'srv> {
-    access: Mutex<AptAccess<'srv>>,
+#[derive(Debug)]
+pub struct AptLock {
+    access: Mutex<AptAccess>,
+    signal_event: Event,
+    resume_event: Event,
 }
 
-impl<'srv> AptLock<'srv> {
-    pub fn init(srv: &'srv mut Srv) -> Result<Self> {
-        let mut access = AptAccess {
-            srv,
-            service_name_index: 0,
-        };
+impl AptLock {
+    pub fn init(srv: &Srv) -> Result<Self> {
+        let mut access = AptAccess::new();
 
-        let apt = access.aquire()?;
+        let mut apt = access.aquire(srv)?;
 
         const FLAGS: u16 = 0x0;
         let mutex = apt.get_lock(FLAGS)?;
 
-        let (_signal_event, _resume_event) = apt.init(
+        let (signal_event, resume_event) = apt.init(
             AppId::Application,
-            AppletAttributes::new()
-                .position(AppPosition::App)
-                .manual_gpu_rights()
-                .manual_dsp_rights(),
+            AppletAttributes::new().position(AppPosition::App),
         )?;
 
         let access = Mutex::const_new(mutex, access);
 
-        Ok(Self { access })
+        Ok(Self {
+            access,
+            signal_event,
+            resume_event,
+        })
     }
 }
 
-impl<'srv> Deref for AptLock<'srv> {
-    type Target = Mutex<AptAccess<'srv>>;
+impl Deref for AptLock {
+    type Target = Mutex<AptAccess>;
 
     fn deref(&self) -> &Self::Target {
         &self.access
